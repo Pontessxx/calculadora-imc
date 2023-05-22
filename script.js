7f75cfa50d64cc2a1fae2683536cadf805c122e9
Pontes <hpo.pontes@gmail.com>
2023-05-22 19:34:36 -0300

    calculo do imc, e pegando valores do input

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event){
     //impede que a pagina recarregue o form
     event.preventDefault();
+
+    const weight = document.getElementById('weight').value; //pega o valor do input
+    const height = document.getElementById('height').value; //pega o valor do input
+
+    const imc = (weight/ (height * height)).toFixed(2);
+    const value = document.getElementById('value');
+    let description = '';
+    
 })
