ecb8df6392ec0dfc3421fe77cd52949f47e3cc57
Pontes <hpo.pontes@gmail.com>
2023-05-22 20:34:07 -0300

    erro ao imprimir valor imc

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
 
     const weight = document.getElementById('weight').value; //pega o valor do input
-    const height = document.getElementById('height').value; //pega o valor do input
+    const height = document.getElementById('height').value.replace(',', '.'); //pega o valor do input
 
-    const imc = (weight/ (height * height)).toFixed(2);
-    const value = document.getElementById('value');
+    const imc = (weight / (height * height)).toFixed(2);
+    const valor = document.getElementById('value');
+
     let description = '';
+
     document.getElementById('infos').classList.remove('hidden');
+
+    if(imc < 18.5){
+        description = 'Voce está abaixo do peso, entrar em contato com uma nutricionista'
+    }
+    valor.textContent = imc
+    document.getElementById('description').textContent = description;
 })
